a9e417bb6a3360f49f401c8b72b3ae53b01dfdf6
rong5026 <rong5026@naver.com>
2022-04-02 13:51:45 +0900

    Score 표시

--- TIC_TAC_TOE/TIC_TAC_TOE/GameStart.cs
+++ TIC_TAC_TOE/TIC_TAC_TOE/GameStart.cs
@@ -23,24 +23,23 @@ namespace TIC_TAC_TOE
             {
 
                 Menu menu = new Menu();
-                int game_mode = menu.ShowMenu();
+                int game_mode = menu.ShowMenu();  // 메뉴 선택  1.computer게임 2.user게임 3.score 4.Exit
 
                
-                if (game_mode == 0)
+                if (game_mode == 0)  // 4.Exit 선택 시 종료
                 {
                     break;    // 프로그램 종료
                 }
-                else if(game_mode == 3)
+                else if(game_mode == 3)  // 3.Score 선택
                 {
                     PlayGame start = new PlayGame();
                     Console.WriteLine("┌------------------------------------------------┐");
                     Console.WriteLine("│        USER SCORE : "+start.score[0]+"    COMPUTER SOCRE :" + start.score[1]+"     │");
                     Console.WriteLine("└------------------------------------------------┘\n"); //score 프린트
                 }
-                else
+                else  // 1 or 2 게임시작
                 {
                     PlayGame start = new PlayGame();
-                    
                     start.GamePlay(game_mode);
                 }
                     
