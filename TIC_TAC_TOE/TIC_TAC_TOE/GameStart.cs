a5cf47d87519bade80dd7590be6e7f6d38f9eb7a
rong5026 <rong5026@naver.com>
2022-04-03 23:10:52 +0900

    매직넘버, 생성자, 변수이름 수정, 출력문 클래스 생성

--- TIC_TAC_TOE/TIC_TAC_TOE/GameStart.cs
+++ TIC_TAC_TOE/TIC_TAC_TOE/GameStart.cs
@@ -14,23 +14,27 @@ namespace TIC_TAC_TOE
             public static int playerScore = 0;
         }
 
+        PrintUI UI = new PrintUI();
+        Menu menu;
+        int gameMode;
+        PlayGame start;
+
         public void StartGame()
         {
-            Console.WriteLine("┌------------------------------------------------┐");
-            Console.WriteLine("│            TIC TAC TOE GAME START !!           │");
-            Console.WriteLine("└------------------------------------------------┘\n");
+            
             while (true)
             {
 
-                Menu menu = new Menu();
-                int game_mode = menu.ShowMenu();  // 메뉴 선택  1.computer게임 2.user게임 3.score 4.Exit
+                UI.PrintGameStart();
+                menu = new Menu();
+                gameMode = menu.ShowMenu();  // 메뉴 선택  1.computer게임 2.user게임 3.score 4.Exit
 
                
-                if (game_mode == 0)  // 4.Exit 선택 시 종료
+                if (gameMode == 0)  // 4.Exit 선택 시 종료
                 {
                     break;    // 프로그램 종료
                 }
-                else if(game_mode == 3)  // 3.Score 선택
+                else if(gameMode == 3)  // 3.Score 선택
                 {
                     PlayGame start = new PlayGame();
                     Console.WriteLine("┌------------------------------------------------┐");
@@ -39,8 +43,8 @@ namespace TIC_TAC_TOE
                 }
                 else  // 1 or 2 게임시작
                 {
-                    PlayGame start = new PlayGame();
-                    start.GamePlay(game_mode);
+                    start = new PlayGame();
+                    start.GamePlay(gameMode);
                 }
                     
 
