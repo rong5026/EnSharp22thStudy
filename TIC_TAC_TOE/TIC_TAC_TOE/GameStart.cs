9c96ce0e426dc0a176e96fbe69de5f7a0d4fc066
rong5026 <rong5026@naver.com>
2022-04-04 01:31:27 +0900

    [UPDATE] 매직넘버 사용

--- TIC_TAC_TOE/TIC_TAC_TOE/GameStart.cs
+++ TIC_TAC_TOE/TIC_TAC_TOE/GameStart.cs
@@ -8,42 +8,45 @@ namespace TIC_TAC_TOE
 {
     internal class GameStart
     {
-        public static class Score
-        {
-            public static int computerScore = 0;
-            public static int playerScore = 0;
-        }
 
-        PrintUI UI = new PrintUI();
-        Menu menu;
+        const int EXIT = 0;
+        const int SCORE = 3;
+
+        PrintUI UI;      
         int gameMode;
         PlayGame start;
+        ValidInput selectMenu;
 
+
+        public GameStart()
+        {
+            UI = new PrintUI();           
+            start = new PlayGame();
+            selectMenu = new ValidInput();
+        }
         public void StartGame()
         {
-            
+            UI.PrintGameStart();
             while (true)
             {
-
-                UI.PrintGameStart();
-                menu = new Menu();
-                gameMode = menu.ShowMenu();  // 메뉴 선택  1.computer게임 2.user게임 3.score 4.Exit
-
                
-                if (gameMode == 0)  // 4.Exit 선택 시 종료
+                UI.PrintMenu();
+                gameMode = selectMenu.ValidNumber();  // 1,2,3,0 값을 예외처리 후 입력
+                // 메뉴 선택  1.computer게임 2.user게임 3.score 4.Exit
+              
+
+
+                if (gameMode == EXIT)  // 4.Exit 선택 시 종료
                 {
                     break;    // 프로그램 종료
                 }
-                else if(gameMode == 3)  // 3.Score 선택
+                else if(gameMode == SCORE)  // 3.Score 선택
                 {
-                    PlayGame start = new PlayGame();
-                    Console.WriteLine("┌------------------------------------------------┐");
-                    Console.WriteLine("│        USER SCORE : "+start.score[0]+"    COMPUTER SOCRE :" + start.score[1]+"     │");
-                    Console.WriteLine("└------------------------------------------------┘\n"); //score 프린트
+                    Console.Clear();
+                    UI.PrintScore(0,0);
                 }
                 else  // 1 or 2 게임시작
-                {
-                    start = new PlayGame();
+                {             
                     start.GamePlay(gameMode);
                 }
                     
