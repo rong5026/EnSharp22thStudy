2f5d3af346db7d42e4a0c8534284d921642d70d1
rong5026 <rong5026@naver.com>
2022-04-04 01:29:12 +0900

    [REFACTOR] UI클래스 생성, UI출력

---
+++ TIC_TAC_TOE/TIC_TAC_TOE/PrintUI.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIC_TAC_TOE
+{
+    internal class PrintUI
+    {
+
+        public void PrintBoard(char[] gameBoard,int gameType)
+        {
+
+            Console.WriteLine("┌---------------------------------------┐");
+            Console.WriteLine("│            ││            ││           │");
+            Console.WriteLine("│            ││            ││           │");
+            Console.WriteLine("│      {0}     ││      {1}     ││     {2}     │", gameBoard[1], gameBoard[2], gameBoard[3]);
+            Console.WriteLine("│            ││            ││           │");
+            Console.WriteLine("│            ││            ││           │");
+            Console.WriteLine("│------------││------------││-----------│");
+            Console.WriteLine("│            ││            ││           │");
+            Console.WriteLine("│            ││            ││           │");
+            Console.WriteLine("│      {0}     ││      {1}     ││     {2}     │", gameBoard[4], gameBoard[5], gameBoard[6]);
+            Console.WriteLine("│            ││            ││           │");
+            Console.WriteLine("│            ││            ││           │");
+            Console.WriteLine("│------------││------------││-----------│");
+            Console.WriteLine("│            ││            ││           │");
+            Console.WriteLine("│            ││            ││           │");
+            Console.WriteLine("│      {0}     ││      {1}     ││     {2}     │", gameBoard[7], gameBoard[8], gameBoard[9]);
+            Console.WriteLine("│            ││            ││           │");
+            Console.WriteLine("│            ││            ││           │");
+            Console.WriteLine("└---------------------------------------┘");
+
+            if(gameType == 1)
+            {
+                Console.WriteLine("│  USER_1 =  [ O ]      USER_2 = [ X ]  │");
+                Console.WriteLine("└---------------------------------------┘");
+            }
+            else
+            {
+                Console.WriteLine("│  USER_1 =  [ O ]    Computer = [ X ]  │");
+                Console.WriteLine("└---------------------------------------┘");
+            }
+         
+        }
+
+        public void PrintGameOver()
+        {
+            Console.WriteLine("┌------------------------------------------------┐");
+            Console.Write("│                  ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("GAME OVER !!!");
+            Console.ResetColor();
+            Console.WriteLine("                 │");
+            Console.WriteLine("└------------------------------------------------┘\n");
+        }
+
+        public void PrintMenu()
+        {
+         
+            Console.WriteLine("┌------------------------------------------------┐");
+            Console.WriteLine("│                 SELECT GAME MODE               │");
+            Console.WriteLine("└------------------------------------------------┘");
+            Console.WriteLine("┌------------------------------------------------┐");
+            Console.WriteLine("│ 1.vs Computer  2.vs User  3.Scoreboard  0.Exit │");
+            Console.WriteLine("└------------------------------------------------┘\n");
+        }
+        public void PrintGameStart()
+        {
+            Console.WriteLine("┌------------------------------------------------┐");
+            Console.WriteLine("│            TIC TAC TOE GAME START !!           │");
+            Console.WriteLine("└------------------------------------------------┘\n");
+        }
+
+        public void PrintGameStop()
+        {
+            Console.WriteLine("┌------------------------------------------------┐");
+            Console.WriteLine("│                  GAME STOP !!!                 │");
+            Console.WriteLine("└------------------------------------------------┘\n");
+        }
+        public void PrintFirstUserWin()
+        {
+            Console.WriteLine("┌------------------------------------------------┐");
+            Console.Write("│                  ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("USER_1 WIN !!!");
+            Console.ResetColor();
+            Console.WriteLine("                │");
+            Console.WriteLine("└------------------------------------------------┘\n");
+        }
+
+        public void PrintSecondUserWin()
+        {
+            Console.WriteLine("┌------------------------------------------------┐");
+            Console.Write("│                  ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("USER_2 WIN !!!");
+            Console.ResetColor();
+            Console.WriteLine("                │");
+            Console.WriteLine("└------------------------------------------------┘\n");
+         
+        }
+        public void PrintDraw()
+        {
+            Console.WriteLine("┌------------------------------------------------┐");
+            Console.Write("│                    ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("DRAW !!!");
+            Console.ResetColor();
+            Console.WriteLine("                    │");
+            Console.WriteLine("└------------------------------------------------┘\n");
+        }
+        public void PrintComputerWin()
+        {
+            Console.WriteLine("┌------------------------------------------------┐");
+            Console.Write("│                 ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Computer WIN !!!");
+            Console.ResetColor();
+            Console.WriteLine("               │");
+            Console.WriteLine("└------------------------------------------------┘\n");
+
+        }
+        public void PrintScore(int userScore, int computerScore)
+        {
+            Console.WriteLine("┌------------------------------------------------┐");
+            Console.WriteLine("│        USER SCORE : " + userScore + "    COMPUTER SOCRE :" + computerScore + "     │");
+            Console.WriteLine("└------------------------------------------------┘\n"); 
+        }
+       
+        public void PrintSelectOtherNumber()
+        {
+            Console.WriteLine("Choose an unselected number from 1 to 9");
+        }
+
+      
+    }
+}
