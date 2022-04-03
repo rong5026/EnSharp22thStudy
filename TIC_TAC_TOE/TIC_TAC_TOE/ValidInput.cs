3698bedb89cb72b426c65aabf49904b371c8def2
rong5026 <rong5026@naver.com>
2022-04-04 01:28:14 +0900

    switch case 사용, UI 클래스로 추가

--- TIC_TAC_TOE/TIC_TAC_TOE/ValidInput.cs
+++ TIC_TAC_TOE/TIC_TAC_TOE/ValidInput.cs
@@ -8,43 +8,49 @@ namespace TIC_TAC_TOE
 {
     internal class ValidInput
     {
+            
+        public const int EXIT = 0;
         public int ValidNumber()  // 입력값 유효성 검사
         {
+            const int EXIT = 0;
+            const int USERMODE = 1;
+            const int COMPUTERMODE = 2;
+            const int SCORE = 3;
+
+            PrintUI UI = new PrintUI();
+            string selectNum;
+            char charNumber;
+            int input;
+
             while (true)
             {
                 Console.Write("Select Menu : ");
-                string Select_Num = Console.ReadLine();   //입력값 
+                selectNum = Console.ReadLine();   //입력값 
 
-                if (Select_Num.Length == 1)  // 입력값의 길이가 1일때 정수 1~4 조건 통과
+                if (selectNum.Length == 1)  // 입력값의 길이가 1일때 정수 1~4 조건 통과
                 {
-                    char Char_Num = Convert.ToChar(Select_Num);
-                    int input = Convert.ToInt32(Char_Num);
-
-                    if (input == 48)  // 0.Exit 선택 했을때  return 0
+                    charNumber = Convert.ToChar(selectNum);
+                    input = Convert.ToInt32(charNumber);
+                    switch (input)
                     {
-                        Console.WriteLine("┌------------------------------------------------┐");
-                        Console.WriteLine("│                  GAME STOP !!!                 │");
-                        Console.WriteLine("└------------------------------------------------┘\n");
-                        return 0;
-                    }
-                    else if (input == 49) // 1. computer Mode 선택 시 return 1.
-                    {
-                        return 1;
-                    }
-                    else if (input == 50)// 2. user Mode 선택 시 return 2.
-                    {
-                        return 2;
-                    }
-                    else if (input == 51)// 3. Score mode 선택 시 return 3.
-                    {
-                        return 3;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Enter an integer between 0 and 3");
-                        return ValidNumber();  // 조건에 적합하지 않은 입력값 받았을 때 다시 리콜
-                    }
-               
+                        case 48:
+                            UI.PrintGameStop(); // gamestopUI 프린트
+                            return EXIT;
+                            break;
+                        case 49:
+                            return USERMODE;
+                            break;
+                        case 50:
+                            return COMPUTERMODE;
+                            break;
+                        case 51:
+                            return SCORE;
+                            break;
+                        default:
+                            Console.WriteLine("Enter an integer between 0 and 3");
+                            return ValidNumber();  // 조건에 적합하지 않은 입력값 받았을 때 다시 리콜
+                            break;
+                    }                     
                 }
                 else
                 {
