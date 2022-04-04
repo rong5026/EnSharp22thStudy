bf72085e28f55a92b50e0215a6988b2a888f6af1
rong5026 <rong5026@naver.com>
2022-04-05 00:18:54 +0900

    [IMPROVE] 조건문 줄임

--- TIC_TAC_TOE/TIC_TAC_TOE/ValidInput.cs
+++ TIC_TAC_TOE/TIC_TAC_TOE/ValidInput.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Collections;
 namespace TIC_TAC_TOE
 {
     internal class ValidInput
     {
             
-        public const int EXIT = 0;
+        private const int EXIT = 0;
         public int ValidNumber()  // 입력값 유효성 검사
         {
             const int EXIT = 0;
@@ -27,32 +27,25 @@ namespace TIC_TAC_TOE
                 Console.Write(" Select Menu : ");
                 selectNum = Console.ReadLine();   //입력값 
 
-                if (selectNum != null)
+
+                if (selectNum != null && selectNum.Length == 1)  // 입력값의 길이가 1일때 정수 1~4 조건 통과
                 {
-                    if (selectNum.Length == 1)  // 입력값의 길이가 1일때 정수 1~4 조건 통과
+                    charNumber = Convert.ToChar(selectNum);
+                    input = Convert.ToInt32(charNumber);
+                    switch (input)
                     {
-                        charNumber = Convert.ToChar(selectNum);
-                        input = Convert.ToInt32(charNumber);
-                        switch (input)
-                        {
-                            case 48:
-                                UI.PrintGameStop(); // gamestopUI 프린트
-                                return EXIT;
-                            case 49:
-                                return USERMODE;
-                            case 50:
-                                return COMPUTERMODE;
-                            case 51:
-                                return SCORE;
-                            default:
-                                PrintValidNumberError();
-                                return ValidNumber();  // 조건에 적합하지 않은 입력값 받았을 때 다시 리콜
-                        }
-                    }
-                    else
-                    {
-                        PrintValidNumberError();
-                        return ValidNumber();  // 새로 입력값, 입력값이 한자리 수가 아닐때 
+                        case 48:
+                            UI.PrintGameStop(); // gamestopUI 프린트
+                            return EXIT;
+                        case 49:
+                            return USERMODE;
+                        case 50:
+                            return COMPUTERMODE;
+                        case 51:
+                            return SCORE;
+                        default:
+                            PrintValidNumberError();
+                            return ValidNumber();  // 조건에 적합하지 않은 입력값 받았을 때 다시 리콜
                     }
                 }
                 else
@@ -69,38 +62,28 @@ namespace TIC_TAC_TOE
             
             char charNumber;
             int input;
-
             string selectNum = Console.ReadLine();
 
-            if (selectNum != null)
+            if (selectNum != null && selectNum.Length == 1)
             {
+                charNumber = Convert.ToChar(selectNum);
+                input = Convert.ToInt32(charNumber);
 
-                if (selectNum.Length == 1)
+                if (input >= 49 && input <= 57) // 1 ~ 9까지의 정수
                 {
-                    charNumber = Convert.ToChar(selectNum);
-                    input = Convert.ToInt32(charNumber);
-
-                    if (input >= 49 && input <= 57) // 1 ~ 9까지의 정수
+                    for (int index = 1; index < 9; index++)
                     {
-                        for (int index = 1; index < 9; index++)
+                        if (gameBoard[index] == 'O' || gameBoard[index] == 'X')
                         {
-                            if (gameBoard[index] == 'O' || gameBoard[index] == 'X')
+                            if (index == (input - '0'))
                             {
-                                if (index == (input - '0'))
-                                {
 
-                                    PrintValidGameInputError(gameBoard, gameType, usernum); // ERROR 프린트
-                                    return ValidGameInput(usernum, gameBoard, gameType);  // 새로 입력값 
-                                }
+                                PrintValidGameInputError(gameBoard, gameType, usernum); // ERROR 프린트
+                                return ValidGameInput(usernum, gameBoard, gameType);  // 새로 입력값 
                             }
                         }
-                        return input - '0';
                     }
-                    else
-                    {
-                        PrintValidGameInputError(gameBoard, gameType, usernum); // ERROR 프린트
-                        return ValidGameInput(usernum, gameBoard, gameType);  // 새로 입력값 
-                    }
+                    return input - '0';
                 }
                 else
                 {
@@ -113,6 +96,9 @@ namespace TIC_TAC_TOE
                 PrintValidGameInputError(gameBoard, gameType, usernum); // ERROR 프린트
                 return ValidGameInput(usernum, gameBoard, gameType);  // 새로 입력값 
             }
+
+
+
 
         }
 
