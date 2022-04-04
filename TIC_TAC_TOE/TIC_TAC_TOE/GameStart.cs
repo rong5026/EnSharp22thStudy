b7ce5201531d403fab95d2dc88658866abb5a05a
rong5026 <rong5026@naver.com>
2022-04-04 22:46:01 +0900

    [ADD] REsetBoard() - board 초기화 함수 추가

--- TIC_TAC_TOE/TIC_TAC_TOE/GameStart.cs
+++ TIC_TAC_TOE/TIC_TAC_TOE/GameStart.cs
@@ -13,16 +13,17 @@ namespace TIC_TAC_TOE
         const int SCORE = 3;
 
         PrintUI UI;      
-        int gameMode;
+        int gameType;
         PlayGame start;
         ValidInput selectMenu;
 
+        char[] gameBoard = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
         public GameStart()
         {
-            UI = new PrintUI();           
-            start = new PlayGame();
+            UI = new PrintUI();     
             selectMenu = new ValidInput();
+            
         }
         public void StartGame()
         {
@@ -31,26 +32,36 @@ namespace TIC_TAC_TOE
             {
                
                 UI.PrintMenu();
-                gameMode = selectMenu.ValidNumber();  // 1,2,3,0 값을 예외처리 후 입력
+                gameType = selectMenu.ValidNumber();  // 1,2,3,0 값을 예외처리 후 입력
                 // 메뉴 선택  1.computer게임 2.user게임 3.score 4.Exit
               
 
 
-                if (gameMode == EXIT)  // 4.Exit 선택 시 종료
+                if (gameType == EXIT)  // 4.Exit 선택 시 종료
                 {
                     break;    // 프로그램 종료
                 }
-                else if(gameMode == SCORE)  // 3.Score 선택
+                else if(gameType == SCORE)  // 3.Score 선택
                 {
                     Console.Clear();
                     UI.PrintScore(0,0);
                 }
                 else  // 1 or 2 게임시작
-                {             
-                    start.GamePlay(gameMode);
+                {
+
+                    ResetBoard(); // Board 초기화
+                    start = new PlayGame(gameBoard);
+                    start.GamePlay(gameType);
                 }
                     
 
+            }
+        }
+        private void ResetBoard() // Board 초기화
+        {
+            for (int i = 0; i < gameBoard.Length; i++)
+            {
+                gameBoard[i] = Convert.ToChar(i + '0');
             }
         }
         
