82c2caeae100900f21e7d44298143d8716c046bd
rong5026 <rong5026@naver.com>
2022-04-07 00:07:03 +0900

    [ADD] MainUI 만듬

---
+++ Library/Library/LibraryUI.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    internal class LibraryUI
+    {
+
+        public void PrintMainUI()
+        {
+			Console.WriteLine("\n");
+			Console.WriteLine("                                                     @@          @@");
+			Console.WriteLine("                                                  (@@              @@)");
+			Console.WriteLine("                     @@)               @@          @:    (:@@:)    :@          @@               (@@");
+			Console.WriteLine("                    @                 :#            @@@@@@0@@0@@@@@@            #:                 @");
+			Console.WriteLine("                   (##@@#:      (#@#<--(@@@@      _-@@            @@-_      @@@@)-->#@#)     :#@@##)");
+			Console.WriteLine("                        (@@@@@@@@@         @@@@@@@                    @@@@@@@         @@@@@@@@@)\n");
+			//
+			Console.WriteLine("                 ####      ####    ########      ########             ###       ########      ####     ####  ");
+			Console.WriteLine("                  ##        ##      ##     ##     ##     ##           ###        ##     ##     ##       ##    ");
+			Console.WriteLine("                  ##        ##      ##    ###     ##      ##         ## ##       ##      ##     ##     ##     ");
+			Console.WriteLine("                  ##        ##      ##  ##        ##     ##         ##   ##      ##     ##       ##  ##        ");
+			Console.WriteLine("                  ##        ##      ##  ##        ##  ###          #########     ##  ###           ###         ");
+			Console.WriteLine("                  ##        ##      ##    ###     ####   ##       ##       ##    ####   ##         ##           ");
+			Console.WriteLine("                  ##        ##      ##     ###    ##       ##    ##         ##   ##       ##       ##           ");
+			Console.WriteLine("                 ########  ####    #########     ####       ### ##           ## ####       ###    ####          \n");
+			//
+			Console.WriteLine("            @@)                                    * L I B R A R Y *                                     (@@");
+			Console.WriteLine("           @     @@         @@@)                                                        (@@@         @@     @");
+			Console.WriteLine("          @    _@  @       @          @@@@@@    _____@@#<<(==)>>#@@_____    @@@@@@          @       @  @_    @");
+			Console.WriteLine("          (#@@#@       @@@@:@@@@@@@@@@  || @@@@@  | @ << (-==-) >> @ |  @@@@@ ||  @@@@@@@@@@:@@@@       @#@@#)");
+			Console.WriteLine("               @@@@@@@@              @@@@@   |  @@@@ --<< (==) >>-- @@@@  |   @@@@@              @@@@@@@@");
+			Console.WriteLine("                                      @   @###@@@  @@@@_        _@@@@  @@@###@   @");
+			Console.WriteLine("                                                       @-  ()  -@");
+			Console.WriteLine("                                                        @------@");
+			Console.WriteLine("                                                         @@__@@");
+
+
+		}
+		public void PrintMainUI1()
+		{
+			Console.WriteLine("\n");
+			Console.WriteLine("                                                     @@          @@");
+			Console.WriteLine("                                                  (@@              @@)");
+			Console.WriteLine("                     @@)               @@          @:    (:@@:)    :@          @@               (@@");
+			Console.WriteLine("                    @                 :#            @@@@@@0@@0@@@@@@            #:                 @");
+			Console.WriteLine("                   (##@@#:      (#@#<--(@@@@      _-@@            @@-_      @@@@)-->#@#)     :#@@##)");
+			Console.WriteLine("                        (@@@@@@@@@         @@@@@@@                    @@@@@@@         @@@@@@@@@)\n");
+			//
+			Console.WriteLine("                       ####    ####   ######   ####      ####        ######   ####           ####");
+			Console.WriteLine("                        ##      ##   ##    ##   ##        ##        ##    ##   ##             ##");
+			Console.WriteLine("                        ##      ##  ##      ##  ##        ##       ##      ##  ##             ##");
+			Console.WriteLine("                        ##########  ##      ##  ##        ##       ##      ##   ##    ###    ##");
+			Console.WriteLine("                        ##      ##  ##      ##  ##        ##       ##      ##   ##   ## ##   ##");
+			Console.WriteLine("                        ##      ##  ##      ##  ##        ##       ##      ##    ## ##   ## ##");
+			Console.WriteLine("                        ##      ##   ##    ##   ##     #  ##     #  ##    ##     ## ##   ## ##");
+			Console.WriteLine("                       ####    ####   ######   ######### #########   ######       ####   ####\n");
+			//
+			Console.WriteLine("            @@)                                    * L I B R A R Y *                                     (@@");
+			Console.WriteLine("           @     @@         @@@)                                                        (@@@         @@     @");
+			Console.WriteLine("          @    _@  @       @          @@@@@@    _____@@#<<(==)>>#@@_____    @@@@@@          @       @  @_    @");
+			Console.WriteLine("          (#@@#@       @@@@:@@@@@@@@@@  || @@@@@  | @ << (-==-) >> @ |  @@@@@ ||  @@@@@@@@@@:@@@@       @#@@#)");
+			Console.WriteLine("               @@@@@@@@              @@@@@   |  @@@@ --<< (==) >>-- @@@@  |   @@@@@              @@@@@@@@");
+			Console.WriteLine("                                      @   @###@@@  @@@@_        _@@@@  @@@###@   @");
+			Console.WriteLine("                                                       @-  ()  -@");
+			Console.WriteLine("                                                        @------@");
+			Console.WriteLine("                                                         @@__@@");
+
+
+		}
+	}
+}
---
+++ TIC_TAC_TOE/TIC_TAC_TOE/GameMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIC_TAC_TOE
+{
+    internal class GameMain
+    {
+        static void Main(string[] args)
+        {
+            GameStart start = new GameStart();
+            start.StartGame();
+
+        }
+
+
+    }
+}
