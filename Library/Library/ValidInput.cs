f404010c8f2f452a16af2a9cd03a9491ee0422df
rong5026 <rong5026@naver.com>
2022-04-07 18:35:12 +0900

    [ADD] login 입력 예외처리

--- Library/Library/ValidInput.cs
+++ Library/Library/ValidInput.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Library
 {
     internal class ValidInput
     {
+        //private string id;
+       // private string password;
+        //private string name;
+        //private int age;
+        //private int number;
+        //private string address;
+
+        string stringInput;
+        int integerInput;
+        bool check;
+        Regex regex;
+
+        string id;
+        string password;
+
+
+        public string EnterId()
+        {
+            Console.SetCursorPosition(17, 5);
+            id = Console.ReadLine();
+            check = Regex.IsMatch(id, @"^[0-9a-zA-Z]{8,10}$");
+
+            if (!check)
+                return EnterId();
+            return id;
+        }
     }
 }
