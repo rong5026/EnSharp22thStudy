75976a856bc277e4c791542143980eaaa82b0559
rong5026 <rong5026@naver.com>
2022-04-07 18:35:43 +0900

    [ADD] Login 기능 수정중

---
+++ Library/Library/Login.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace Library
+{
+
+    internal class Login
+    {
+        
+        ValidInput validInput =  new ValidInput();
+        LibraryUI UI = new LibraryUI();
+        string id;
+        string password;
+      
+        public bool UserManagerLogin(List<UserVO> list)
+        {
+            Console.Clear();
+            UI.PrintLogin(); // 위에 로그인 화면 
+
+          
+            id = validInput.EnterId();
+
+            return true;
+
+
+        }
+    }
+}
