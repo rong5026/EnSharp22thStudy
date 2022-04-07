8af15848d01a6044d5629463456870fc78c7a3d9
rong5026 <rong5026@naver.com>
2022-04-08 08:36:05 +0900

    [ADD] 회원가입 입력값 예외처리

--- Library/Library/Register.cs
+++ Library/Library/Register.cs
@@ -7,43 +7,113 @@ using System.Threading.Tasks;
 namespace Library
 {
     internal class Register
+
     {
+        const int EIXT = -1;
+        const int ID =1;
+        const int PASSWORD =2;
+        const int REPASSWORD =3;
+        const int NAME = 4;
+        const int AGE=5;
+        const int PHONE_NUMBER=6;
+        const int ADDRESS =7;
+
         LibraryUI UI = new LibraryUI();
         ValidInput validInput = new ValidInput();
+        UserVO user = new UserVO();
         ConsoleKeyInfo keyInput;
         string id;
         string password;
+        string repassword;
         string name;
         string age;
         string phoneNumber;
         string address;
+       
+        string[] userDate = new string[7];
 
-
-
-        public bool UserRegister()
+        public bool RegistUser(List<UserVO> list)
         {
 
             Console.Clear();
             UI.PrintRegister();
             Console.SetCursorPosition(38, 7);
-            keyInput = Console.ReadKey(true);
 
-            if (keyInput.Key == ConsoleKey.Escape)
-                return false; // ESC 누르면 뒤로가기 
-            else
+            //user정보 입력.
+            userDate[0] = EnterUserDate(list,null,"id");
+            userDate[1] = EnterUserDate(list, null, "password");
+            userDate[2] = EnterUserDate(list, userDate[1],"repassword");
+            userDate[3] = EnterUserDate(list, null, "name");
+            userDate[4] = EnterUserDate(list, null, "age");
+            userDate[5] = EnterUserDate(list, null, "phonenumber");
+            userDate[6] = EnterUserDate(list, null, "address");
+
+            user.Id = userDate[0];
+            user.Password = password;
+            for (int index = 0; index < 7; index++)
             {
-                id = validInput.EnterIdOrPassword(38,7);
-                password = validInput.EnterIdOrPassword(38, 8);
-                password = validInput.EnterIdOrPassword(38, 9);
-                name = validInput.EnterUserName();
-                age = validInput.EnterUserAge();
-                phoneNumber = validInput.EnterUserPhoneNumber();
-                address = validInput.EnterUserAddress();
+                if(userDate[index] == "EIXT")
+                    return false;
+                else
+                {
+                    for(int listindex = 0; listindex < list.Count; listindex++)
+                    {
+                        if(list[listindex].Id == userDate[0])
+                        {
+                            UI.PrintIdOverlap(); // 이미 존재하는 id 출력
+                            return false;
+                        }
+                    }
 
+                    
+
+
+                }
             }
-           
+
+
+
 
             return true;
         }
+
+        public string EnterUserDate(List<UserVO> list,string password,string type)
+        {
+           
+            keyInput = Console.ReadKey(true);
+            if (keyInput.Key == ConsoleKey.Escape)
+                return "EIXT"; // ESC 누르면 뒤로가기 
+            else
+            {
+                switch (type)
+                {
+                    case "id":
+                        id = validInput.EnterId(list,38, 7);
+                        return id; 
+                    case "password":
+                        password = validInput.EnterIdOrPassword(38, 8);
+                        return password;
+                    case "repassword":
+                        repassword = validInput.EnterRepassword(password,38, 9);
+                        return password;
+                    case "name":
+                        name = validInput.EnterUserName();
+                        return name;
+                    case "age":
+                        age = validInput.EnterUserAge();
+                        return age;
+                    case "phonenumber":
+                        phoneNumber = validInput.EnterUserPhoneNumber();
+                        return phoneNumber;
+                    case "address":
+                        address = validInput.EnterUserAddress();
+                        return address;
+                    default:
+                        return "EIXT";
+                }
+
+            }
+        }
     }
 }
+
--- Library/Library/ValidInput.cs
+++ Library/Library/ValidInput.cs
@@ -10,30 +10,109 @@ namespace Library
     internal class ValidInput
     {
         //private string id;
-       // private string password;
+        // private string password;
         //private string name;
         //private int age;
         //private int number;
         //private string address;
-
+   
+ 
         string stringInput;
         int integerInput;
         bool check;
-        Regex regex;
 
         string id;
-        string password;
+        string passoword;
+        string repassword;
+        string idPassword;
+        string name;
+        string age;
+        string phoneNumber;
+        string address;
 
-
-        public string EnterId()
+        public string EnterIdOrPassword(int x,int y) // 좌표
         {
-            Console.SetCursorPosition(17, 5);
-            id = Console.ReadLine();
-            check = Regex.IsMatch(id, @"^[0-9a-zA-Z]{8,10}$");
-
+           
+            Console.SetCursorPosition(x, y);
+           
+            idPassword = Console.ReadLine();
+            if (idPassword != null)
+                check = Regex.IsMatch(idPassword, @"^[0-9a-zA-Z]{8,15}$"); // 숫자 영어 8~15글자
             if (!check)
-                return EnterId();
-            return id;
+                return EnterIdOrPassword(x,y);
+            return idPassword;
         }
+       
+        public string EnterId(List<UserVO> list,int x, int y)
+        {
+            id = EnterIdOrPassword(x, y);
+
+            for(int index = 0; index < list.Count; index++)
+            {
+                list[index].Id = id;
+                return EnterId(list,x,y);
+            }
+
+            return id;
+
+        }
+        public string EnterRepassword(string password,int x,int y)
+        {
+            repassword = EnterIdOrPassword(x, y);
+
+            if(repassword != password)
+                return EnterRepassword(password,x,y);
+            else
+                return repassword;
+        }
+
+
+        public string EnterUserName() // user이름 예외검사
+        {
+            Console.SetCursorPosition(41, 10);
+            name = Console.ReadLine();
+            if (name != null)
+                check = Regex.IsMatch(name, @"^[a-zA-Zㄱ-ㅎ가-힣]{2,}$"); // 영어,한글 2글자이상
+            if (!check)
+                return EnterUserName();
+            return name;
+
+        }
+        public string EnterUserAge() // 나이 예외검사
+        {
+            Console.SetCursorPosition(39, 11);
+            age = Console.ReadLine();
+            if (age != null)
+                check = Regex.IsMatch(age, @"^1?[0-9]?[0-9]$"); // 0~ 199
+            if (!check)
+                return EnterUserAge();
+            return age;
+
+           
+        }
+        public string EnterUserPhoneNumber() // 휴대폰번호 예외검사
+        {
+            Console.SetCursorPosition(41, 12);
+            phoneNumber = Console.ReadLine();
+            if (phoneNumber != null)
+                check = Regex.IsMatch(phoneNumber, @"01([0-9]{1})-([0-9]{4})-([0-9]{4})$"); // 01로 시작0~9숫자중 한개 오고 0~9 숫자 4개-0~9 숫자4개
+            if (!check)
+                return EnterUserPhoneNumber();
+            return phoneNumber;
+           
+        }
+        public string EnterUserAddress() // 주소 예외검사.
+        {
+            Console.SetCursorPosition(38, 13);
+            address = Console.ReadLine();
+            if (address != null)
+                check = Regex.IsMatch(address, @"[ㄱ-ㅎ가-힣]{3,}$"); //한글+숫자 주소 최소 3단어 이상
+            if (!check)
+                return EnterUserAddress();
+            return phoneNumber;
+        }
+
+     
+
     }
 }
