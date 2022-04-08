41b35b273c17092db881c0cf0f9a03dd3b9db524
rong5026 <rong5026@naver.com>
2022-04-08 09:24:31 +0900

    [ADD] 로그인 후 메뉴

---
+++ Library/Library/UserMenu.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    internal class UserMenu
+    {
+        const bool PROGRAM_ON = true;
+        LibraryUI UI = new LibraryUI();
+        SelectionMode mode = new SelectionMode();
+
+        int menuNumber;
+
+
+        public void StartUserMenu()
+        {
+            UI.PrintMainUI();
+            Console.SetWindowSize(125, 40);
+
+            UI.PrintUserMenuUI(1);
+            while (PROGRAM_ON)
+            {
+                menuNumber = mode.SelectUserMenu();// 도서찾기, 도서대여,대여도서확인,회원정보수정
+
+                switch (menuNumber)
+                {
+                    case USER_MODE: // 도서찾기
+                        userMode.StartUserMode(userList);
+                        break;
+                    case MANAGE_MODE: // 도서대여
+                        break;
+                    case STOP: //대여도서확인
+                        UI.PrintProgramStop();
+                        break;
+                    case decimal: // 회원정보 수정
+                        break;
+                    case 회원탈퇴: // 회원탈퇴
+
+
+                }
+
+            }
+            
+
+        }
+    }
+}
