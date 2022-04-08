54f08f89289881b21a11b8465d3fb5dc2b4877c6
rong5026 <rong5026@naver.com>
2022-04-08 14:15:35 +0900

    [ADD] 예시DATA 추가, stackoverflow 오류 수정

--- Library/Library/LibraryStart.cs
+++ Library/Library/LibraryStart.cs
@@ -3,73 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Text.RegularExpressions;
 namespace Library
 {
+
     internal class LibraryStart
     {
-        LibraryUI UI = new LibraryUI();
-        const Boolean PROGRAM_ON = true;
 
+        public static List<UserVO> userList = new List<UserVO>();
+        public static List<BookVO> bookList = new List<BookVO>();
+
+
+        LibraryUI UI = new LibraryUI();
+        SelectionMode mode = new SelectionMode();
+        UserMode userMode = new UserMode();
+        UserVO user;
+        BookVO book;
+
+        const Boolean PROGRAM_ON = true;
+        const int USER_MANAGER = 1;
+        const int LOGIN_REGISTER = 2;
+        int menuNumber;
         const int USER_MODE = 1;
         const int MANAGE_MODE = 2;
         const int STOP = -1;
-        int menuNumber;
-
-   
-        ConsoleKeyInfo keyInput;
 
 
+        public LibraryStart()
+        {
+            user = new UserVO(null, null, null, null, null, null);  //현재 로그인한 계정
+            userList.Add(user);
+
+            user = new UserVO("hong1234", "hong1234", "홍영환", "23", "010-8791-4859", "서울시"); // 계정1
+            userList.Add(user);
+            user = new UserVO("abcd1234", "abcd1234", "피카츄", "21", "010-1234-1234", "서울시 오류동"); // 계정2
+            userList.Add(user);
+
+            book = new BookVO(BookVO.totalBook, "자료구조 및 실습", "국형준", "세종대", 2, 14000, "2020-02-02", 0);
+
+            bookList.Add(book);
+            book = new BookVO(BookVO.totalBook, "노래방가고싶다", "홍영환", "세종대", 1, 10000, "2020-04-08", 0);
+            bookList.Add(book);
+            book = new BookVO(BookVO.totalBook, "너두? 나두", "황기태", "생능출판", 3, 20000, "2021-03-02", 0);
+            bookList.Add(book);
+            book = new BookVO(BookVO.totalBook, "자바스크립트", "고경희", "이지스퍼블리싱", 1, 8000, "2022-11-05", 0);
+            bookList.Add(book);
+
+        }
         public void StartProgram()
         {
+       
             UI.PrintMainUI();
             Console.SetWindowSize(125, 40);
-
+          
             while (PROGRAM_ON)
             {
-                menuNumber = SelectMode();
-
+                menuNumber = mode.SelectMode(USER_MANAGER); // 회원모드 . 유저모드 선택
                 switch (menuNumber)
                 {
-                    case USER_MODE:
+                    case USER_MODE: // 유저모드 일때
+                        userMode.StartUserMode(userList);
                         break;
-                    case MANAGE_MODE:
+                    case MANAGE_MODE: // 관리자 모드
                         break;
                     case STOP:
-                        UI.PrintProgramStop();
+                        UI.PrintProgramStop(); // 종료
                         return;
-                        break;
+
 
                 }
 
             }
-            
+
         }
 
-        public int SelectMode()
-        {
-            UI.PrintSelectMainUI(USER_MODE);
-            while (PROGRAM_ON)
-            {                       
-                keyInput = Console.ReadKey(true);
-                switch (keyInput.Key)
-                {              
-                    case ConsoleKey.UpArrow:
-                        menuNumber = USER_MODE;
-                        UI.PrintSelectMainUI(USER_MODE);
-                        break;
 
-                    case ConsoleKey.DownArrow:
-                        menuNumber = MANAGE_MODE;
-                        UI.PrintSelectMainUI(MANAGE_MODE);
-                        break;
-                    case ConsoleKey.Enter:
-                        return menuNumber;
-                    case ConsoleKey.Escape:
-                        return STOP;
-                }
-            }
-        }
     }
-       
+
 }
--- Library/Library/UserMenu.cs
+++ Library/Library/UserMenu.cs
@@ -9,6 +9,12 @@ namespace Library
     internal class UserMenu
     {
         const bool PROGRAM_ON = true;
+        const int BOOK_SEARCH = 1;
+        const int BOOK_RENT = 2;
+        const int BOOK_BORROW_LIST = 3;
+        const int USER_EDIT = 4;
+        const int DELETE_ID = 5;
+        const int EXIT = -1;
         LibraryUI UI = new LibraryUI();
         SelectionMode mode = new SelectionMode();
 
@@ -27,23 +33,28 @@ namespace Library
 
                 switch (menuNumber)
                 {
-                    case USER_MODE: // 도서찾기
-                        userMode.StartUserMode(userList);
+                    case BOOK_SEARCH: // 도서찾기
+                        //userMode.StartUserMode(userList);
                         break;
-                    case MANAGE_MODE: // 도서대여
+                    case BOOK_RENT: // 도서대여
                         break;
-                    case STOP: //대여도서확인
+                    case BOOK_BORROW_LIST: //대여도서확인
                         UI.PrintProgramStop();
                         break;
-                    case decimal: // 회원정보 수정
+                    case USER_EDIT: // 회원정보 수정
                         break;
-                    case 회원탈퇴: // 회원탈퇴
+                    case DELETE_ID: // 회원탈퇴
+                        break;                   
+                    case EXIT:
+                        return;
+                    default:
+                        return;
 
 
                 }
 
             }
-            
+
 
         }
     }
