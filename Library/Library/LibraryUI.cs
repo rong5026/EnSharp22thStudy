a4b5cd7956c59ca275148c51b801d881bbe7b48b
rong5026 <rong5026@naver.com>
2022-04-08 11:57:09 +0900

    [ADD] 출력 UI 추가

--- Library/Library/LibraryUI.cs
+++ Library/Library/LibraryUI.cs
@@ -11,7 +11,8 @@ namespace Library
 		const Boolean PROGRAM_ON = true;
 		const int WIDTH = 120;
 		const int FIRST_TYPE = 1;
-		
+		string selectFirst;
+		string selectSecond;
 
 
 		public void PrintMainUI()
@@ -52,8 +53,8 @@ namespace Library
 		
 		public void PrintSelectUI(int selectNum,int type)  // type ->1.회원모드,관리자모드   2.로그인,회원가입
 		{
-			string selectFirst = selectNum == 1 ? "●" : "○";
-			string selectSecond = selectNum == 2 ? "●" : "○";
+			selectFirst = selectNum == 1 ? "●" : "○";
+			selectSecond = selectNum == 2 ? "●" : "○";
 
 			Console.Clear();
 			PrintMainUI();  // main 이미지 출력
@@ -91,7 +92,67 @@ namespace Library
 			Console.WriteLine("                                   └------------------------------------------------┘");
 
 			Console.WriteLine("                                         ↑ 또는 ↓ 키를 눌러 메뉴를 이동하세요.");
+		}
+		public void PrintUserMenuUI(int selectNum)
+        {
+			Console.Clear();
+			PrintMainUI();  // main 이미지 출력
+			Console.WriteLine("                                   ┌------------------------------------------------┐");
+			Console.WriteLine("                                   │                                                │");
 
+			Console.Write("                                   │                 ");
+			if (selectNum == 1)
+			{
+				Console.ForegroundColor = ConsoleColor.Green;
+				selectFirst = "●";
+			}
+			else
+				selectFirst = "○";
+			Console.Write("{0}  도서찾기", selectFirst);
+			Console.ResetColor();
+			Console.WriteLine("                   │");
+
+
+			Console.Write("                                   │                 ");
+			if (selectNum == 2)
+			{
+				Console.ForegroundColor = ConsoleColor.Green;
+				selectFirst = "●";
+			}
+			else
+				selectFirst = "○";
+			Console.Write("{0}  도서대여", selectFirst);
+			Console.ResetColor();
+			Console.WriteLine("                   │");
+
+			Console.Write("                                   │                 ");
+			if (selectNum == 3)
+			{
+				Console.ForegroundColor = ConsoleColor.Green;
+				selectFirst = "●";
+			}
+			else
+				selectFirst = "○";
+			Console.Write("{0}  도서대여확인", selectFirst);
+			Console.ResetColor();
+			Console.WriteLine("               │");
+
+			Console.Write("                                   │                 ");
+			if (selectNum == 4)
+			{
+				Console.ForegroundColor = ConsoleColor.Green;
+				selectFirst = "●";
+			}
+			else
+				selectFirst = "○";
+			Console.Write("{0}  회원정보수정", selectFirst);
+			Console.ResetColor();
+			Console.WriteLine("               │");
+
+			Console.WriteLine("                                   │                                                │");
+			Console.WriteLine("                                   └------------------------------------------------┘");
+
+			Console.WriteLine("                                         ↑ 또는 ↓ 키를 눌러 메뉴를 이동하세요.");
 
 
 		}
@@ -143,19 +204,35 @@ namespace Library
 			Console.WriteLine("│                                                │");
 			Console.WriteLine("│          (뒤로가려면 ESC를 눌러주세요)         │");
 			Console.WriteLine("└------------------------------------------------┘\n");
-			Console.WriteLine(" User ID (8~ 15글자 영어, 숫자포함) : ");
-			Console.WriteLine(" User PW (8~ 15글자 영어, 숫자포함) : ");
-			Console.WriteLine(" User PW  (     Password 확인     ) : ");
-			Console.WriteLine(" User Name (한글,영어 포함 2글자 이상) :");
-			Console.WriteLine(" User Age (   자연수 1세 ~ 200세   ) :");
-			Console.WriteLine(" User PhoneNumber (   01xxxxxxxx  ) :");
-			Console.WriteLine(" User Address (     한글 주소     ) :");
-
-
-
+			Console.WriteLine(" User ID (8~ 15글자 영어, 숫자포함) : ");//38 7
+			Console.WriteLine(" User PW (8~ 15글자 영어, 숫자포함) : ");//38 8
+			Console.WriteLine(" User PW  (     Password 확인     ) : ");//38 9
+			Console.WriteLine(" User Name (한글,영어 포함 2글자 이상) : ");//41 10
+			Console.WriteLine(" User Age (   자연수 1세 ~ 200세   ) : ");//39 11
+			Console.WriteLine(" User PhoneNumber (   01x-xxxx-xxxx  ) : ");//41 12
+			    Console.Write(" User Address (     한글 주소     ) : ");//38 13
 
 		}
-	
+		
+		public void PrintSuccessRegister()
+        {
+			Console.WriteLine("┌------------------------------------------------┐");
+			Console.WriteLine("│                                                │");
+			Console.WriteLine("│               회 원 가 입 성 공 !!             │");
+			Console.WriteLine("│                                                │");
+			Console.WriteLine("│              (ENTER을 눌러주세요 !)            │");
+			Console.WriteLine("└------------------------------------------------┘\n");
+		}
+		public void PrintSuccessLogin()
+		{
+			Console.WriteLine("┌------------------------------------------------┐");
+			Console.WriteLine("│                                                │");
+			Console.WriteLine("│                 로 그 인 성 공 !!              │");
+			Console.WriteLine("│                                                │");
+			Console.WriteLine("│              (ENTER을 눌러주세요 !)            │");
+			Console.WriteLine("└------------------------------------------------┘\n");
+		}
+
 		public void PrintCenter(string message)
         {
 			Console.WriteLine(String.Format(message).PadLeft(WIDTH - (WIDTH / 2 - (message.Length / 2))));
