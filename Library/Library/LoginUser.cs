ca42f5e692e9ab39d6288499ceaf3caeb774bf2a
rong5026 <rong5026@naver.com>
2022-04-08 20:17:01 +0900

    [ADD] 현재 로그인한 유저의 index 리턴

---
+++ Library/Library/LoginUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    internal class LoginUser
+    {
+
+        public int SearchLoginUser() // 로그인한 사람의 index를 return함
+        {
+
+            for (int index = 1; index < LibraryStart.userList.Count; index++)
+            {
+                if (LibraryStart.userList[0].Id == LibraryStart.userList[index].Id)
+                    return index;
+            }
+            return -1;
+        }
+
+    }
+}
