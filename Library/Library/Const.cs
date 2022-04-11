015d93b35ae3c36d76217f84effe6f309005c51d
rong5026 <rong5026@naver.com>
2022-04-11 12:51:28 +0900

    [ADD] cons클래스 추가

---
+++ Library/Library/Const.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    internal class Const
+    {
+
+        const int LOGIN_REGISTER = 2;
+        const int LOGIN_MODE = 1;
+        const int REGISTER_MODE = 2;
+        const int STOP = -1;
+        const int BACK = -1;
+        const int LOGIN_FAIL = 0;
+        const int LOGIN_SUCCESS = 1;
+
+    }
+}
