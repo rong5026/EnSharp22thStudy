7eb5ffc3ed2852ea2543d9c27fc21888820a4e96
rong5026 <rong5026@naver.com>
2022-04-13 12:58:22 +0900

    LTT 시작 , 메인UI

---
+++ LTT/LTT/View/LoginUI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTT
+{
+    internal class LoginUI
+    {
+
+       
+        public void PrintLoginUI()
+        {
+            Console.SetWindowSize(120, 30);
+
+          
+            Console.WriteLine("\n\n");
+            Console.WriteLine("============================== Sejong University =============================="); //40개
+            Console.WriteLine("\n");
+            Console.WriteLine("▣      ▣▣▣    ▣▣▣  ▣▣▣▣  ▣      ▣");
+            Console.WriteLine("▣     ▣    ▣  ▣          ▣     ▣▣    ▣");
+            Console.WriteLine("▣     ▣    ▣  ▣   ▣     ▣     ▣  ▣  ▣");
+            Console.WriteLine("▣     ▣    ▣  ▣    ▣    ▣     ▣    ▣▣");
+            Console.WriteLine("▣▣▣  ▣▣▣    ▣▣▣  ▣▣▣▣  ▣      ▣\n\n");
+
+
+
+            Console.WriteLine("==============================================================================="); //40개
+
+        }
+
+
+        
+
+    }
+}
