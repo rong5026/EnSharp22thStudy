f8704ddcf769995276f390e33080b9ac8ac2a5cc
rong5026 <rong5026@naver.com>
2022-04-13 19:03:22 +0900

    [UPDATE] selectionMenu.cs  SelectUserMenu() case문 안에 함수로 뱀

--- LTT/LTT/Controller/SelectionMenu.cs
+++ LTT/LTT/Controller/SelectionMenu.cs
@@ -10,6 +10,8 @@ namespace LTT
     internal class SelectionMenu
     {
         int menuNumber;
+        int menu;
+        int input;
         UserMenuUI userMenuUI = new UserMenuUI();
         //LoginUI loginUI = new LoginUI();
         //Exception exceuption = new Exception();
@@ -23,18 +25,11 @@ namespace LTT
                 keyInput = Console.ReadKey(true);
                 switch (keyInput.Key)
                 {
-                    case ConsoleKey.UpArrow:
-                        menuNumber--;
-                        if (menuNumber == -1)
-                            menuNumber = menuCount - 1;
-
-                        userMenuUI.PrintUserMenu((menuNumber % menuCount) + 1);
-                    
+                    case ConsoleKey.UpArrow: // 위로가기 키
+                        menuNumber=SetMenuNumber(menuNumber, menuCount, "UP");
                         break;
-                    case ConsoleKey.DownArrow:
-                        menuNumber++;
-
-                        userMenuUI.PrintUserMenu((menuNumber % menuCount) + 1);
+                    case ConsoleKey.DownArrow: // 아래로가기 키
+                        menuNumber=SetMenuNumber(menuNumber, menuCount, "DOWN");
                         break;
                     case ConsoleKey.Enter:
                         return (menuNumber % menuCount) + 1;
@@ -42,6 +37,20 @@ namespace LTT
                         return Constant.STOP;
                 }
             }
+        }
+        private int SetMenuNumber(int menuNumber,int menuCount, string type)
+        {
+            input = menuNumber;
+            if (type == "UP") { input--; }
+            else { input++; }
+
+            if(input == -1)
+                input = menuCount - 1;
+
+
+            userMenuUI.PrintUserMenu((input % menuCount) + 1);
+
+            return input; 
         }
     }
 }
