ac55ff059669594bcf68b47b65a9551ae3b854b3
hong <rong5026@naver.com>
2022-05-02 16:38:57 +0900

    [TEST] 바탕화면에 파일저장 테스트 완료

--- LibraryMySQL/LibraryMySQL/LibraryMain.cs
+++ LibraryMySQL/LibraryMySQL/LibraryMain.cs
@@ -12,9 +12,14 @@ namespace LibraryMySQL
     {            
         static void Main(string[] args)
         {
-            LibraryStart start = new LibraryStart();
-            start.StartLibrary();
-         
+            //LibraryStart start = new LibraryStart();
+            //start.StartLibrary();
+            //   Excel.Workbook workbook = application.Workbooks.Open(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "\\2022년도 1학기 강의시간표.txt");
+
+            //File.Create(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "\\Library로그기록.txt");
+
+            File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "\\Library로그기록.txt", "안녕");
+           
            
         }
     }
