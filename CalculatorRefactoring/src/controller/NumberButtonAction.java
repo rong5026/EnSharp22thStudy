609652d1c8aee405ad02494ce08113d5bc5feb04
rong5026 <rong5026@naver.com>
2022-05-18 21:24:53 +0900

    [ADD] 숫자 버튼클릭, 콤마, 소수점 엑션 추가

--- CalculatorRefactoring/src/controller/NumberButtonAction.java
+++ CalculatorRefactoring/src/controller/NumberButtonAction.java
@@ -29,7 +29,6 @@ public class NumberButtonAction {
 			}
 			else if(index ==17) { // 숫자 0		
 				button[index].addActionListener(new ActionListener() {
-					
 					@Override
 					public void actionPerformed(ActionEvent e) {
 						pressedButton = (JButton)e.getSource();
@@ -37,7 +36,53 @@ public class NumberButtonAction {
 					}			
 				});
 			}
-			
+			else if(index ==18) { // 소수점 입력
+				button[index].addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {	
+						pressedButton = (JButton)e.getSource();
+						enterDotAction(pressedButton.getText());
+					}			
+				});
+			}
+			else if(index == 0) { // CE 입력
+				button[index].addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {				
+						enterCEAction();
+					}			
+				});
+			}
+			else if(index == 1) { //C 입력
+				button[index].addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						enterCAction();
+						
+					}			
+				});
+			}
+		
+			else if(index == 2) { //back 입력
+				button[index].addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						enterBackAction();
+					}			
+				});
+			}
+			else if(index == 16) { //± 입력
+				button[index].addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {				
+						enterNegateAction();
+					}			
+				});
+			}
+			else if(index == 3 ||index == 7||index == 11||index == 15|| index ==19) { // 나누기 , 곱하기, 빼기 , 더하기, 등호 
+				
+				
+			}
 			// 키보드 포커싱 주기
 			CalculatorStart.mainFrame.setFocusable(true);
 			CalculatorStart.mainFrame.requestFocus();
@@ -72,6 +117,63 @@ public class NumberButtonAction {
 			CalculatorStart.inputNumber = CalculatorStart.inputNumber+input;
 			changeFontSize(CalculatorStart.inputNumber,"Down");
 			TextPanel.inputJLabel.setText(setComma(CalculatorStart.inputNumber));
+			
+		}
+	}
+	public void enterDotAction(String input) { // . 소수점 입력
+		if(TextPanel.inputJLabel.getText()=="0" || CalculatorStart.inputNumber=="" ) {
+			CalculatorStart.inputNumber = "0.";
+			TextPanel.inputJLabel.setText(CalculatorStart.inputNumber);
+		}
+		// .을 쓴적이 없고, 최대길이 이전일때
+		else if(TextPanel.inputJLabel.getText().contains(input) ==false &&CalculatorStart.inputNumber.length()<16) {
+		
+			CalculatorStart.inputNumber = CalculatorStart.inputNumber+input;
+			changeFontSize(CalculatorStart.inputNumber,"Down");
+			TextPanel.inputJLabel.setText(setComma(CalculatorStart.inputNumber));
+		}
+	}
+	public void enterCEAction() { //CE 입력
+		CalculatorStart.inputNumber = "";				
+		TextPanel.inputJLabel.setFont(new Font("맑은 고딕", Font.BOLD , 55 ));						
+		TextPanel.inputJLabel.setText("0");
+		
+	}
+	public void enterCAction() { // C입력
+		
+		CalculatorStart.inputNumber = "";		
+		CalculatorStart.previousNumber = "";		
+		TextPanel.inputJLabel.setFont(new Font("맑은 고딕", Font.BOLD , 55 ));		
+		TextPanel.inputJLabel.setText("0");
+		TextPanel.previousJLabel.setText("");
+	}
+	public void enterBackAction() {
+		if(CalculatorStart.inputNumber.length()>0) {
+			
+			//입력값 1개 삭제
+			CalculatorStart.inputNumber = CalculatorStart.inputNumber.substring(0,CalculatorStart.inputNumber.length()-1);
+						
+			changeFontSize(CalculatorStart.inputNumber,"Up");
+			
+		
+			TextPanel.inputJLabel.setText(setComma(CalculatorStart.inputNumber));
+			}	
+			if(CalculatorStart.inputNumber.length() ==0) {
+				CalculatorStart.inputNumber ="";
+				TextPanel.inputJLabel.setText("0");
+			}
+	}
+	public void enterNegateAction() { //± 입력
+		if(CalculatorStart.inputNumber!="" && TextPanel.inputJLabel.getText()!="0") {
+			
+			
+			if( Double.parseDouble(CalculatorStart.inputNumber) <0) 
+				CalculatorStart.inputNumber = CalculatorStart.inputNumber.replace("-", "");
+		
+			else 		
+				CalculatorStart.inputNumber = "-"+CalculatorStart.inputNumber;
+				
+			TextPanel.inputJLabel.setText(setComma(CalculatorStart.inputNumber) );
 			
 		}
 	}
