bf2b339b462c7d35114d57aeb950070d3126ba01
rong5026 <rong5026@naver.com>
2022-05-18 20:39:31 +0900

    [UPDATE] TextPanel 레이아웃 Gridbaglayout으로 변경

--- CalculatorRefactoring/src/view/TextPanel.java
+++ CalculatorRefactoring/src/view/TextPanel.java
@@ -3,6 +3,8 @@ package view;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JLabel;
@@ -20,10 +22,13 @@ public class TextPanel extends JPanel{
 	public TextPanel() {
 	
 		
-		setLayout(new GridLayout(3,1)); // 상하 3개로 나누기
-		setPreferredSize(new Dimension(500,180));
+		setLayout(new GridBagLayout()); // 상하 3개로 나누기
+		GridBagConstraints grdBagConstraints =new GridBagConstraints();
+		grdBagConstraints.fill = GridBagConstraints.BOTH;
 		
 		setBackground(new Color(220,220,220));
+	
+		
 		
 		previousJLabel = new JLabel("");
 		inputJLabel = new JLabel("0");
@@ -32,19 +37,26 @@ public class TextPanel extends JPanel{
 		previousJLabel.setFont(new Font("맑은 고딕", 0, 15 ));
 		previousJLabel.setForeground(new Color(64,64,64));
 		previousJLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		previousJLabel.setOpaque(true);
-		previousJLabel.setBackground(Color.red);
 		
 		//입력 값
 		inputJLabel.setFont(new Font("맑은 고딕", Font.BOLD , 55));
 		inputJLabel.setForeground(Color.BLACK);
 		inputJLabel.setHorizontalAlignment(SwingConstants.RIGHT);
 		
-	
+		grdBagConstraints.weightx = 1;
+		grdBagConstraints.weighty = 0.1;
+		grdBagConstraints.gridx = 0;
+		grdBagConstraints.gridy = 0;
+		add(previousJLabel,grdBagConstraints);
+		
+		grdBagConstraints.weightx = 1;
+		grdBagConstraints.weighty = 0.3;
+		grdBagConstraints.gridx = 0;
+		grdBagConstraints.gridy = 1;
+		add(inputJLabel,grdBagConstraints);
 	
 		
-		add(previousJLabel);
-		add(inputJLabel);
+		
 	}
 	
 	
