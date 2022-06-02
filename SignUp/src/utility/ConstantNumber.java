766df993882acb0153b9920246c6eb4dfe4598f4
rong5026 <rong5026@naver.com>
2022-06-02 14:02:43 +0900

    [ADD] 왼쪽 위 아이콘 주가

--- SignUp/src/utility/ConstantNumber.java
+++ SignUp/src/utility/ConstantNumber.java
@@ -1,5 +1,9 @@
 package utility;
 
 public class ConstantNumber {
+	
+	
+	public final static String SIGNUP_ICON_IMAGE  = "./src/image/LegoIcon.png";
 
+	
 }
--- SignUp/src/view/MainFrame.java
+++ SignUp/src/view/MainFrame.java
@@ -1,6 +1,11 @@
 package view;
 
+import java.awt.BorderLayout;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+
+import utility.ConstantNumber;
 
 public class MainFrame {
 	
@@ -16,19 +21,31 @@ public class MainFrame {
 		
 		mainFrame = new JFrame();
 		
-		mainFrame.setTitle("EN# SignUp");     //타이틀 이름 
+		setMainFrame(); // main frame 초기설정
 		
-		mainFrame.setSize(1400,900);//프레임의 크기
+		mainFrame.setLayout(new BorderLayout()); // 창 레이아웃 
 		
-		mainFrame.setResizable(false);        //창 크기 고정
-		
-		mainFrame.setLocationRelativeTo(null);//창 가운데 배치
 		
 		mainFrame.setVisible(true);
 		
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//JFrame이 정상적으로 종료되게
 		
 		 
+	}
+	
+	
+	private void setMainFrame() { // mainframe 초기설정
+		
+		ImageIcon icon = new ImageIcon(ConstantNumber.SIGNUP_ICON_IMAGE);
+		mainFrame.setIconImage(icon.getImage());
+		
+		mainFrame.setTitle("EN# SignUp");       //타이틀 이름 
+		
+		mainFrame.setSize(1400,900);			//프레임의 크기
+		
+		mainFrame.setResizable(false);           //창 크기 고정
+		
+		mainFrame.setLocationRelativeTo(null);   //창 가운데 배치
 	}
 
 }
