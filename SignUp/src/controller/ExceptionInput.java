a9d1b2cf1385222f6a3fac901ad47f9577b73580
rong5026 <rong5026@naver.com>
2022-06-02 16:57:10 +0900

    [ADD] mainframe버튼달기

---
+++ SignUp/src/controller/ExceptionInput.java
@@ -0,0 +1,28 @@
+package controller;
+
+import javax.print.attribute.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
+
+import view.MainFrame;
+
+public class ExceptionInput {
+	
+	private MainFrame dFrame = new MainFrame();
+	
+	
+	public void d() {
+		
+		
+		
+		
+	}
+	
+
+	
+	
+	
+	
+}
+
+
--- SignUp/src/view/MainFrame.java
+++ SignUp/src/view/MainFrame.java
@@ -9,8 +9,10 @@ import java.awt.Image;
 import java.awt.TextField;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 import utility.ConstantNumber;
@@ -53,7 +55,7 @@ public class MainFrame extends JFrame{
 		
 		setTitle("EN# SignUp");       //타이틀 이름 
 		
-		setSize(900,1100);			//프레임의 크기
+		setSize(800,900);			//프레임의 크기
 		
 		setResizable(false);           //창 크기 고정
 		
@@ -70,41 +72,118 @@ class MainPanel extends JPanel{
 	
 	private ImageIcon backgroundIcon;
 	private Image background;
-	private TextField idText;
-	private TextField passwordText;
+	private JTextField idText;
+	private JPasswordField passwordText;
+	private JButton login;
+	private JButton signUp;
+	private JButton idFinding;
+	private JButton passwordFinding;
 
 	public MainPanel() {
 
-		idText = new TextField();
-		passwordText = new TextField();
+		idText = new JTextField();
+		
+		passwordText = new JPasswordField();
 		
 		
-		setMainPanelComponent();
+		login = new JButton("Login");
+		
+		signUp = new JButton("Sign Up");
+		
+		idFinding = new JButton("Find ID");
+		
+		passwordFinding = new JButton("Find PW");
+		
+		
+		setMainInput(); // id, password TextField 설정
+		
+		setMainButton();
+		
+		
+		add(idText);
+		
+		add(passwordText);
+		
+		
+		
+		
+		
 		setLayout(null);
+		
 	}
 	
 	
 	private void setMainPanelComponent() { // mainPanel에 textfield, button 배치
 		
-		passwordText.setEchoChar('*'); // 비밀번호 입력시 *처리
-
+		setMainInput(); 
+		 
+		 
 		add(idText);
+		
 		add(passwordText);
+		
+		
+	}
+	
+	
+	private void setMainInput() {  // id, password TextField 설정
+		
+		Font font = new Font("맑은고딕",Font.PLAIN,25);
+		
+		idText.setFont(font);
+		
+		passwordText.setFont(font); // 폰트 설정
+		
+		
+		idText.setHorizontalAlignment(JTextField.CENTER); 
+		
+		passwordText.setHorizontalAlignment(JTextField.CENTER); // 입력 가운데 정렬
+
+		
+		idText.setBounds(250,500,300,40);
+		
+		passwordText.setBounds(250,550,300,40); // 위치설정
+		
+		
+		idText.requestFocus(); // 키보드 포커스 가져오기
+
+		 //JFormattedTextField
+		
+		
+	}
+	
+	private void setMainButton() {
+		
+		Font buttonFont = new Font("Stencil",Font.BOLD,30);
+		
+		
+		setButtonOption(login,buttonFont);
+		
+		setButtonOption(signUp,buttonFont);
+		
+		setButtonOption(idFinding,buttonFont);
+		
+		setButtonOption(passwordFinding,buttonFont);
+		
+		
+		
+		add(idFinding);
+		
+		idFinding.setBounds(50,50,300,100);
 		
 		
 		
 	}
 	
-	private void setInputTextFile() {  // 입력 TextField 설정
+	private void setButtonOption(JButton button,Font font) { //버튼 폰트적용
 		
-		Font font = new Font("맑은고딕",Font.PLAIN,30);
+		button.setBorderPainted(false); //테두리 제거
 		
-		passwordText.setEchoChar('*'); // 비밀번호 입력시 *처리
-		passwordText.setFont(font);
-		passwordText.setBounds(10,20,12,12);
+		button.setFocusPainted(false); //선택시 사각형생기는거 제거
 		
 		
-		idText.setFont(font);
+		button.setFont(font);
+		
 		
 	}
 	
