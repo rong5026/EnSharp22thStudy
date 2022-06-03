3a6311a5f2781616514180142d43952ef4b1b1f1
rong5026 <rong5026@naver.com>
2022-06-03 20:17:29 +0900

    [ADD] Jdbs 파일 다운 후 추가

--- SignUp/src/view/SignupPanel.java
+++ SignUp/src/view/SignupPanel.java
@@ -1,13 +1,20 @@
 package view;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.border.LineBorder;
 
 import controller.ComponentEdit;
+import utility.ConstantNumber;
 
 public class SignupPanel extends UserEditPanel{
 
+	private ImageIcon backgroundIcon;
+	private Image background;
+	
 	public SignupPanel(ComponentEdit componentEdit) {
 		
 		super(componentEdit);
@@ -20,12 +27,20 @@ public class SignupPanel extends UserEditPanel{
 				
 		this.editButton.setText("Sign Up"); //버튼 문구변경
 		
-	
-		
-	
-		
-		
 		
 	}
 
+
+	public void paintComponent(Graphics graphic) {  // mainframe 배경화면 설정
+		
+		
+		super.paintComponent(graphic); // frame의 크기를 받기 위해 super
+		
+		this.backgroundIcon = new ImageIcon(ConstantNumber.SIGNUP_PANEL_IMAGE);
+		
+		this.background= backgroundIcon.getImage();   // mainframe 배경이미지
+		
+		graphic.drawImage(background, 0, 0,getWidth(),getHeight(),this); 
+		
+	}
 }
